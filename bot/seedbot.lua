d28ea0bb663c831b70026520fc6e54a3264b36b4
Godfather021 <MYWAY_AUST@YAHOO.COM>
2016-01-15 23:58:48 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -222,29 +222,29 @@ function create_config( )
     "leave_ban",
     "block_user"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {128556958,111020322,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
+    about_text = [[Lionbot (Teleseed) v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
 
 https://github.com/SEEDTEAM/TeleSeed
 
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
+@mehran_hpr
+.
+.
+.
 
 Special thanks to
 awkward_potato
 Siyanew
 topkecleon
-Vamptacus
+Vamptacus and iman daneshi 
 
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+.
+.
+.
 ]],
     help_text_realm = [[
 Realm Commands:
